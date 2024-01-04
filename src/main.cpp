b1a89ea243ff85593f9380e62b12f5f2ae765d5f
Skyfighter64 <skyfighter64@protonmail.com>
2024-01-04 21:46:50 +0100

    Fix order of configuration values in constructor

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,19 @@
 #include <Arduino.h>
 #include <FastLED.h>
 //#include "UdpConnection.h"
-#include "SerialConnection.h"
-#include "ALUP.h"
+#include "ALUP/SerialConnection.h"
+#include "ALUP/ALUP.h"
 
 //#include "WiFi_Credentials.h"
 
 #define NUM_LEDS 10
-#define DATA_PIN 12
-#define CLOCK_PIN 13
+#define DATA_PIN 13
+#define CLOCK_PIN 12
 
 CRGB leds[NUM_LEDS];
 
 
-Alup alup(leds, DATA_PIN, CLOCK_PIN, NUM_LEDS);
+Alup alup(leds, NUM_LEDS, DATA_PIN, CLOCK_PIN);
 //UdpConnection connection = UdpConnection(SSID, PASSWORD, "192.168.178.35", 5012);
 SerialConnection connection = SerialConnection(115200);
 
